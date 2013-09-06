66e9600404bb1831d0853ff9022ee322cb8f5355
rsgoncalves <goncalvjr@gmail.com>
2013-09-06 03:04:10 +0100

    Revert 8ba56ba..b6d123d
    This rolls back to commit 8ba56bae0fe74ebe12bdf4d87b739d82fc49d7cd.

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -18,12 +18,14 @@
 ******************************************************************************/
 
 // Array of categories
-var allDivs = new Array("st", "stnt", "add", "addnt", "newdesc", "stequiv", "stequivnt", "arewrite", "aprw", "ared", "areshuf", "aprospnewred", 
-"weak", "weakrt", "rem", "remrt", "retdesc", "wkequiv", "wkequivrt", "rrewrite", "rprw", "rred", "rreshuf", "rprospnewred");
+var allDivs = new Array("st", "stnt", "add", "addnt", "newdesc", "stequiv", "stequivnt", "aweak", "arewrite",
+"aprw", "ared", "aavred", "apseudopred", "weak", "weakrt", "rem", "remrt", "retdesc", "wkequiv", "wkequivrt",
+"rst", "rrewrite", "rprw", "rred", "ravred", "rpsnovred");
+
 var effAds = new Array("st", "stnt", "add", "addnt", "newdesc", "stequiv", "stequivnt");
-var inefAds = new Array("arewrite", "aprw", "ared", "areshuf", "aprospnewred");
+var inefAds = new Array("aweak", "arewrite", "aprw", "ared", "aavred", "apseudopred");
 var effRems = new Array("weak", "weakrt", "rem", "remrt", "retdesc", "wkequiv", "wkequivrt");
-var inefRems = new Array("rrewrite", "rprw", "rred", "rreshuf", "rprospnewred");
+var inefRems = new Array("rst", "rrewrite", "rprw", "rred", "ravred", "rpsnovred");
 
 var allTriggers = new Array('additions', 'effAddsTrigger', 'effAdds', 'ineffAddsTrigger',
 'ineffAdds', 'removals', 'effRemsTrigger', 'effRems', 'ineffRemsTrigger', 'ineffRems');
