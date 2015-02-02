83f2328cb6b0ac822e85e30543e5ea3cc2ba4ba0
Rafael Goncalves <goncalvjr@gmail.com>
2015-02-02 11:03:36 -0800

    rm fpp jni setters

--- setenv.bat
+++
@@ -1,1 +0,0 @@
-set JAVA_OPTS=-Dfactpp.jni.path=\path\to\tomcat\webapps\fpp1.6.2\libFaCTPlusPlusJNI.jnilib -Xmx8G
--- setenv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-export JAVA_OPTS="-Dfactpp.jni.path=/path/to/tomcat/webapps/fpp1.6.2/libFaCTPlusPlusJNI.jnilib -Xmx8G"
